5656f4602640a99a430d554226b64e4bf9656eba
Adrian Kirsten <adrian.kirsten@traaidmark.com>
2017-09-24 18:13:50 +0200

    new config system

--- app/config.js
+++ app/config.js
@@ -4,11 +4,15 @@
 
 // 1. DEPENDENCIES +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-const config = require('yaml-config');
-const settings = config.readConfig('../app.yml')
+const yaml = require('js-yaml')
+const fs = require('fs')
+const e = yaml.load(fs.readFileSync('./app.yml'))
+
+console.log(e)
 
 // 1. END ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+console.log(process.env.NODE_ENV)
 
-module.exports = settings
+module.exports = e
 
 // END OF FILE #################################################################
